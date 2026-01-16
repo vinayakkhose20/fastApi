638784885b0168eb3800c0cbf66ebd1022774429
vinayakkhose20 <vinayakkhose20@gmail.com>
2026-01-16 11:43:17 +0530

    Update database.py
    database url

--- database.py
+++ database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_url="postgresql://postgres:123@localhost:5432/kisanseds"
-engine=create_engine(db_url)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine=create_engine(DATABASE_URL)
 
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
 Base = declarative_base()
